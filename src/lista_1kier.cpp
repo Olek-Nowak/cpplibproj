123d3b5ec604cdd110ca766f296660e8439d1e9f
Olek-Nowak <ANowak001@onet.pl>
2021-03-16 01:40:13 +0100

    Uzupelniono wszystkie definicje dotyczace list jednokierunkowych

--- src/lista_1kier.cpp
+++ src/lista_1kier.cpp
@@ -1,97 +1,212 @@
 #include "lista_1kier.h"
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 template <class T>
-struct lista1kElem {
-    lista1kElem(T dane) {
-        nast = nullptr;
-        dane_ = dane;
+lista1kElem<T>::lista1kElem(T dane) {
+    nast_ = nullptr;
+    dane_ = dane;
 
-    }
-
-};
+}
 
 template <class T>
-class lista_1kier {
+lista_1kier<T>::lista_1kier() {
+    glowa_ = nullptr;
+    ogon_ = nullptr;
 
-    lista_1kier() {
-        głowa_ = nullptr;
+}
+
+template <class T>
+lista_1kier<T>::lista_1kier(string sciezka) {
+    glowa_ = nullptr;
+    ogon_ = nullptr;
+    T temp;
+    ifstream plik = ifstream(sciezka);
+    while (!plik.eof()) {
+        plik >> temp;
+        this->dodaj(temp);
+
+    }
+}
+
+template <class T>
+lista_1kier<T>::~lista_1kier() {
+    lista1kElem<T> *temp;
+    while (glowa_ != nullptr) {
+        temp = glowa_;
+        glowa_ = glowa_->nast_;
+        delete temp;
+
+    }
+    glowa_ = nullptr;
+    ogon_ = nullptr;
+}
+
+template <class T>
+int lista_1kier<T>::druk() {
+    if (glowa_ == nullptr)
+        cout << "Lista jest pusta.\n";
+    lista1kElem<T> *temp = glowa_;
+    while (temp != nullptr) {
+        cout << temp->dane_ << '\n';
+        temp = temp->nast_;
+
+    }
+    return 0;
+
+}
+
+template <class T>
+int lista_1kier<T>::druk(string sciezka) {
+    ofstream plik = ofstream(sciezka);
+    lista1kElem<T> *temp = glowa_;
+    while (temp != nullptr) {
+        plik << temp->dane_ << '\n';
+        temp = temp->nast_;
+
+    }
+    return 0;
+
+}
+
+template <class T>
+int lista_1kier<T>::dodaj(T dane) {
+    lista1kElem<T> *n = new lista1kElem<T>(dane);
+    if (glowa_ == nullptr) {
+        glowa_ = n;
+        ogon_ = n;
+
+    }
+    else {
+        ogon_->nast_ = n;
+        ogon_ = n;
+
+    }
+    return 0;
+
+}
+
+template <class T>
+int lista_1kier<T>::dodaj(T dane, int pozycja) {
+    if (pozycja < 0)
+        return -2;
+    else if (pozycja == 0) {
+        lista1kElem<T> *n = new lista1kElem<T>(dane);
+        n->nast_ = glowa_;
+        glowa_ = n;
+
+    }
+    else {
+        lista1kElem<T> *temp = glowa_;
+        for (int i = 1; i < pozycja; i++) {
+            if (temp == nullptr)
+                return -1;
+            temp = temp->nast_;
+
+        }
+        lista1kElem<T> *n = new lista1kElem<T>(dane);
+        if (temp->nast_ == nullptr)
+            ogon_ = n;
+        lista1kElem<T> *t = temp->nast_;
+        temp->nast_ = n;
+        n->nast_ = t;
+
+    }
+    return 0;
+
+}
+
+template <class T>
+int lista_1kier<T>::usun() {
+    if (glowa_ == nullptr)
+        return -1;
+    else if (glowa_ == ogon_) {
+        delete glowa_;
         ogon_ = nullptr;
+        glowa_ = nullptr;
 
     }
+    else {
+        lista1kElem<T> *temp = glowa_;
+        while (temp->nast_ != ogon_)
+            temp = temp->nast_;
+        temp->nast_ = nullptr;
+        delete ogon_;
+        ogon_ = temp;
 
-    lista_1kier(string ścieżka) {
-        głowa_ = nullptr;
-        ogon_ = nullptr;
-        T temp;
-        ifstream plik = ifstream(ścieżka);
-        while(!plik.eof()) {
-            plik >> temp;
-            this->dodaj(temp);
+    }
+    return 0;
+
+}
+
+template <class T>
+int lista_1kier<T>::usun_napotkany(T dane) {
+    if (glowa_->dane_ == dane) {
+        if (glowa_ == ogon_)
+            ogon_ = nullptr;
+        lista1kElem<T> *temp = glowa_;
+        glowa_ = temp->nast_;
+        delete temp;
+        glowa_ = nullptr;
+
+    }
+    else {
+        lista1kElem<T> *temp = glowa_;
+        if (temp->nast_ == nullptr)
+            return -1;
+        while (temp->nast_->dane_ != dane) {
+            if (temp->nast_ == nullptr)
+                return -1;
+            temp = temp->nast_;
 
         }
+        if (temp->nast_ == ogon_)
+            ogon_ = temp;
+        lista1kElem<T> *t = temp->nast_;
+        temp->nast_ = t->nast_;
+        delete t;
 
     }
+    return 0;
 
-    ~lista_1kier() {
-        lista1kElem* temp;
-        while(głowa_ != nullptr) {
-            temp = głowa_;
-            głowa_ = głowa_->nast;
-            delete temp;
+}
 
+template <class T>
+T lista_1kier<T>::usun(int pozycja) {
+    if (pozycja < 0)
+        return NULL;
+    else if (pozycja == 0) {
+        if (glowa_ == ogon_)
+            ogon_ = nullptr;
+        lista1kElem<T> *temp = glowa_;
+        glowa_ = temp->nast_;
+        delete temp;
+        glowa_ = nullptr;
+
+    }
+    else {
+        lista1kElem<T> *temp = glowa_;
+        for (int i = 1; i < pozycja; i++) {
+            if (temp->nast_ == nullptr)
+                return NULL;
+            temp = temp->nast_;
+            
         }
-        głowa_ = nullptr;
-        ogon_ = nullptr;
+        if (temp->nast_ == ogon_)
+            ogon_ = temp;
+        lista1kElem<T> *t = temp->nast_;
+        temp->nast_ = t->nast_;
+        T wynik = t->dane_;
+        delete t;
+        return wynik;
 
     }
 
-    druk() {
-        lista1kElem* temp = głowa_;
-        while(temp != nullptr) {
-            cout << temp->dane;
-            temp = temp->nast;
+}
 
-        }
-
-    }
-
-    druk(string ścieżka) {
-        ofstream plik = ofstream(ścieżka);
-        lista1kElem* temp = głowa_;
-        while(temp != nullptr) {
-            plik << temp->dane;
-            temp = temp->nast;
-
-        }
-
-    }
-
-    dodaj(T dane) {
-
-
-    }
-
-    dodaj(T dane, int pozycja) {
-
-
-    }
-
-    usuń() {
-
-
-    }
-
-    usuń(T dane) {
-
-
-    }
-
-    usuń(int pozycja) {
-
-
-    }
-
-};
+template class lista_1kier<int>;
+template class lista_1kier<char>;
+template class lista_1kier<float>;
+template class lista_1kier<string>;
--- src/lista_1kier.h
+++ src/lista_1kier.h
@@ -1,10 +1,11 @@
 #ifndef LISTA1K
 #define LISTA1K
+#include <string>
 
 template <class T>
 struct lista1kElem {
     T dane_;
-    lista1kElem* nast;
+    lista1kElem* nast_;
     lista1kElem(T dane);
 
 };
@@ -15,21 +16,21 @@ struct lista1kElem {
 template <class T>
 class lista_1kier {
 private:
-    lista1kElem* głowa_;
-    lista1kElem* ogon_;
-    ~lista_1kier();
+    lista1kElem<T>* glowa_;
+    lista1kElem<T>* ogon_;
 
 public:
+    ~lista_1kier();
     /// Tworzy pustą listę jednokierunkową.
     lista_1kier();
     /// Tworzy pustą listę jednokierunkową i wypełnia ją danymi z pliku.
-    /// @param ścieżka Lokalizacja pliku.
-    lista_1kier(string ścieżka);
+    /// @param sciezka Lokalizacja pliku.
+    lista_1kier(std::string sciezka);
     /// Wypisuje zawartość listy do konsoli.
     int druk();
     /// Zapisuje zawartość listy do pliku.
-    /// @param ścieżka Lokalizacja pliku.
-    int druk(string ścieżka);
+    /// @param sciezka Lokalizacja pliku.
+    int druk(std::string sciezka);
     /// Dodaje nowy element do końca listy.
     /// @param dane Wartość dodawanego elementu.
     int dodaj(T dane);
@@ -38,15 +39,15 @@ public:
     /// @param pozycja Indeks pozycji, poczynając od 0.
     int dodaj(T dane, int pozycja);
     /// Usuwa ostatni element listy.
-    int usuń();
+    int usun();
     /// Usuwa pierwszy napotkany element listy o danej wartości.
     /// @param dane Wartość szukanego elementu do usunięcia.
     /// @returns Zwraca indeks pozycji usuniętego elementu.
-    int usuń(T dane);
+    int usun_napotkany(T dane);
     /// Usuwa element na określonej pozycji.
     /// @param pozycja Indeks pozycji, poczynając od 0.
     /// @returns Zwraca wartość usuniętego elementu.
-    T usuń(int pozycja);
+    T usun(int pozycja);
 
 };
 
