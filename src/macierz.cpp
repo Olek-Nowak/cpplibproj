7d9b9361dd7644e185d99942974763a74b03c5f8
Olek-Nowak <ANowak001@onet.pl>
2021-03-22 22:52:48 +0100

    Dodano kilka dodatkowych metod zwiazanych z macierzami

--- src/macierz.cpp
+++ src/macierz.cpp
@@ -80,24 +80,115 @@ void macierz<T>::druk(string sciezka) {
         plik << '\n';
 
     }
+    plik.close();
+
+}
+
+template <class T>
+macierz<T> macierz<T>::minor(int x, int y) {
+    macierz<T> m = macierz<T>(n_-1, m_-1);
+    bool inx = false, iny;
+    for(int i = 0; i < n_-1; i++) {
+        iny = false;
+        if(i == x)
+            inx = true;
+        for(int j = 0; j < m_-1; j++) {
+            if(j == y)
+                iny = true;
+            m(i, j) = tab_[i+inx][j+iny];   
+
+        }
+
+    }
+    return m;
 
 }
 
 template <class T>
 void macierz<T>::transpozycja() {
+    T** m = new T* [m_];
+    for(int i = 0; i < m_; i++)
+        m[i] = new T [n_];
+    for(int i = 0; i < n_; i++) {
+        for(int j = 0; j < m_; j++)
+            m[j][i] = tab_[i][j];
 
+    }
+    this->~macierz();
+    int temp = m_;
+    m_ = n_;
+    n_ = temp;
+    tab_ = m;
 
 }
 
 template <class T>
-void macierz<T>::obrot() {
+void macierz<T>::przesun_wiersz(int x, int ilosc) {
+    if(x < 0 || x >= n_)
+        return;
+    if(ilosc < 0) {
+        int l = (-1*ilosc) % m_;
+        for(int i = 0; i < l; i++) {
+            T temp;
+            for(int j = 1; j < m_; j++) {
+                temp = tab_[x][j-1];
+                tab_[x][j-1] = tab_[x][j];
+                tab_[x][j] = temp;
 
+            }
+
+        }
+
+    }
+    else {
+        int l = ilosc % m_;
+        for(int i = 0; i < ilosc; i++) {
+            T temp;
+            for(int j = m_-1; j > 0; j--) {
+                temp = tab_[x][j];
+                tab_[x][j] = tab_[x][j-1];
+                tab_[x][j-1] = temp;
+
+            }
+
+        }
+
+    }
 
 }
 
 template <class T>
-int macierz<T>::wyznacznik() {
+void macierz<T>::przesun_kolumne(int y, int ilosc) {
+    if(y < 0 || y >= m_)
+        return;
+    if(ilosc < 0) {
+        int l = (-1*ilosc) % n_;
+        for(int i = 0; i < -1*ilosc; i++) {
+            T temp;
+            for(int j = 1; j < n_; j++) {
+                temp = tab_[j-1][y];
+                tab_[j-1][y] = tab_[j][y];
+                tab_[j][y] = temp;
 
+            }
+
+        }
+
+    }
+    else {
+        int l = ilosc % n_;
+        for(int i = 0; i < ilosc; i++) {
+            T temp;
+            for(int j = n_-1; j > 0; j--) {
+                temp = tab_[j][y];
+                tab_[j][y] = tab_[j-1][y];
+                tab_[j-1][y] = temp;
+
+            }
+
+        }
+
+    }
 
 }
 
--- src/macierz.h
+++ src/macierz.h
@@ -4,7 +4,7 @@
 
 /// Implementacja dynamicznej tablicy dwuwymiarowej.
 
-/// Standardowa dynamiczna struktura danych, alokacja i dealokacja pamięci przebiegają automatycznie.
+/// Standardowa dynamiczna struktura danych, alokacja i dealokacja pamięci przebiegają automatycznie. Przeciążony operator () pozwala na dostęp do poszczególnych pól macierzy.
 template <class T>
 class macierz {
 private:
@@ -23,22 +23,31 @@ public:
     macierz(int n);
     /// Tworzy macierz o wymiarach n x m oraz wypełnia ją danymi z pliku.
     /// @param sciezka Lokalizacja pliku.
+    /// @param n Ilość wierszy macierzy.
+    /// @param m Ilość kolumn macierzy.
     macierz(std::string sciezka, int n, int m);
     /// Wypisuje zawartość macierzy do konsoli.
     void druk();
     /// Zapisuje zawartość macierzy do pliku.
     /// @param sciezka Lokalizacja pliku.
     void druk(std::string sciezka);
+    /// Tworzy kopię macierzy z wykreślonym rzędem x i kolumną y.
+    /// @warning Jedynie dla macierzy kwadratowej, o wymiarach n x n.
+    /// @returns Minor macierzy początkowej.
+    /// @param x Numer wiersza do wykreślenia, poczynając od 0.
+    /// @param y Numer kolumny do wykreślenia, poczynając od 0.
+    macierz minor(int x, int y);
     /// Dokonuje transpozycji macierzy.
     /// @warning Jedynie dla macierzy kwadratowej, o wymiarach n x n.
     void transpozycja();
-    /// Dokonuje obrócenia macierzy.
-    /// @warning Jedynie dla macierzy kwadratowej, o wymiarach n x n.
-    void obrot();
-    /// Oblicza wartość wyznacznika macierzy.
-    /// @warning Jedynie dla macierzy kwadratowej, o wymiarach n x n.
-    /// @returns Wartość wyznacznika, NULL dla macierzy prostokątnej.
-    int wyznacznik();
+    /// Dokonuje przesunięcia cyklicznego wiersza macierzy.
+    /// @param x Numer wiersza do przesunięcia, poczynając od 0.
+    /// @param ilosc Ilość miejsc do przesunięcia, ujemna dla przesunięcia w lewo.
+    void przesun_wiersz(int x, int ilosc);
+    /// Dokonuje przesunięcia cyklicznego kolumny macierzy.
+    /// @param y Numer kolumny do przesunięcia, poczynając od 0.
+    /// @param ilosc Ilość miejsc do przesunięcia, ujemna dla przesunięcia ku górze.
+    void przesun_kolumne(int y, int ilosc);
 
 };
 
